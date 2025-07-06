f258e780d89f386459f6a7021a2c2ec19747caa4
hyeonseo kang <hansx2079@naver.com>
2025-07-06 16:39:51 +0900

    [Gold V] Title: 레벨 햄버거, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16974. 레벨 햄버거/레벨 햄버거.cc
+++ 백준/Gold/16974. 레벨 햄버거/레벨 햄버거.cc
@@ -6,31 +6,33 @@ using namespace std;
 int N;
 long long X;
 long long burgerIngredientCnts[51] = {0};
+long long patty[51] = {0};
+long long burn[51] = {0};
+long long burger[51] = {0};
+
 unordered_map<long long, long long> dp[51];
-// 2, 7
-// B BPPPB P BPPPB B
-// B P P P B
+
 long long solve(int n, long long x){
     if(n == 0 && x == 1)
         return 1;
     
-    if(dp[n].find(x) != dp[n].end())
-        return dp[n][x];
-    
-    long long prevLevelIngredientCnt = burgerIngredientCnts[n - 1];
+    long long prevBurger = burger[n - 1];
     long long result = 0;
-    if(x > 1){
-        result += solve(n-1, min(x-1,
-                                 prevLevelIngredientCnt));
+    if(x > 1 && x < prevBurger + 1){
+        result += solve(n-1, x - 1);
     }
-    if(x > prevLevelIngredientCnt + 1){
+    if(x >= prevBurger + 1){
+        result += patty[n-1];
+    }
+    if(x > prevBurger + 1){
         result ++; // 패티 한장 추가
     }
-    if(x > prevLevelIngredientCnt + 2){
-        result += solve(n-1, min(x - prevLevelIngredientCnt - 2,
-                                 prevLevelIngredientCnt));
+    if(prevBurger + 2 < x && x < prevBurger * 2 + 2){
+        result += solve(n-1, x - prevBurger - 2);
     }
-    dp[n][x] = result;
+    if(x >= burger[n] - 1){
+        result += patty[n-1];
+    }
     return result;
 }
 int main(){
@@ -38,10 +40,13 @@ int main(){
     cin.tie(0);
     
     cin >> N >> X;
+    patty[0] = 1;
+    burger[0] = 1;
     
-    burgerIngredientCnts[0] = 1;
     for(int i=1;i<=N;i++){
-        burgerIngredientCnts[i] = burgerIngredientCnts[i-1] * 2 + 3;
+        patty[i] = patty[i-1] * 2 + 1;
+        burn[i] = burn[i-1] * 2 + 2;
+        burger[i] = patty[i] + burn[i];
     }
     
     cout << solve(N, X);
