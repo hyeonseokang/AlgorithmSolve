6fedb74a4b1c73c7a7d5aeabe7aa1c7598ec5b4e
hyeonseo kang <hansx2079@naver.com>
2025-07-11 15:32:57 +0900

    [Gold IV] Title: 육각수, Time: 32 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/1229. 육각수/육각수.cc
+++ 백준/Gold/1229. 육각수/육각수.cc
@@ -4,38 +4,54 @@
 using namespace std;
 
 constexpr int HEXA_SIZE = 708;
+int HEXA_CNT = 0;
 int hexagon[HEXA_SIZE] = {0};
 
 int n;
 int dp[1000001] = {0};
 
+bool solve(int num, int cnt, int maxCnt){
+    if(num == 0 && cnt == maxCnt)
+        return true;
+    else if(num <= 0 || cnt >= maxCnt)
+        return false;
+    
+    for(int i=0;i<HEXA_CNT;i++){
+        int hexaNum = hexagon[i];
+        
+        if(num < hexaNum)
+            break;
+        
+        if(solve(num - hexaNum, cnt + 1, maxCnt))
+            return true;
+    }
+    
+    return false;
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     
+    cin >> n;
     hexagon[0] = 1;
+    HEXA_CNT = 1;
     
     for(int i=1;i<HEXA_SIZE;i++){
         hexagon[i] = hexagon[i-1] + 4 * i + 1;
+        if(hexagon[i] > n) break;
+        HEXA_CNT++;
     }
     
-    cin >> n;
     
-    int currentHexa = 1;
-    dp[1] = 1;
-    for(int i=2;i<=n;i++){
-        dp[i] = INT_MAX;
-        if(hexagon[currentHexa] == i){
-            dp[i] = 1;
-            currentHexa++;
-        }
-        for(int j=0;j<currentHexa;j++){
-            int hexaIdx = hexagon[j];
-            dp[i] = min(dp[i], dp[hexaIdx] + dp[i- hexaIdx]);
+    int m = 6;
+    
+    for(int i=1;i<=m;i++){
+        if(solve(n, 0, i)){
+            cout << i << "\n";
+            break;
         }
     }
-    
-    cout << dp[n] << "\n";
     
     return 0;
 }
