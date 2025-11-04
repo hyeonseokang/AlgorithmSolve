33cc1c76d075edecf72e60e9cfc524e09cb4087c
hyeonseo kang <hansx2079@naver.com>
2025-11-04 15:26:14 +0900

    [Gold III] Title: 나이트 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16884. 나이트 게임/나이트 게임.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+
+		if (n % 2 == 0) {
+			cout << "cubelover\n";
+		}
+		else {
+			cout << "koosaga\n";
+		}
+	}
+	return 0;
+}
