f5932a930c9f696a20a8c112f1e002a3951f178f
hyeonseo kang <hansx2079@naver.com>
2025-11-07 14:32:19 +0900

    [Gold III] Title: 기숙사 재배정, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/10978. 기숙사 재배정/기숙사 재배정.cc
+++ 백준/Gold/10978. 기숙사 재배정/기숙사 재배정.cc
@@ -3,28 +3,26 @@
 using namespace std;
 
 int t;
-long long dp[21][2] = {0};
+long long dp[21] = {0};
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
 	cin >> t;
-	dp[1][0] = 0;
-	dp[1][1] = 1;
-	dp[2][0] = 1;
-	dp[2][1] = 0;
+	dp[1] = 0;
+	dp[2] = 1;
 
 	for (int i = 3; i <= 20; i++) {
-		dp[i][0] = dp[i - 1][0] * (i - 1) + dp[i - 1][1];
-		dp[i][1] = i * dp[i - 1][0];
+		int prev = i - 1;
+		dp[i] = dp[prev] * (prev) + dp[i - 2] * (prev);
 	}
 
 	while(t--) {
 		int n;
 		cin >> n;
 
-		cout << dp[n][0] << "\n";
+		cout << dp[n] << "\n";
 	}
 	return 0;
 }
