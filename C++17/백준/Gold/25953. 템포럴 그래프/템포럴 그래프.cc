2fc18e510a88334960b42893104552398ee0dc3b
hyeonseo kang <hansx2079@naver.com>
2025-12-07 16:38:32 +0900

    [Gold III] Title: 템포럴 그래프, Time: 216 ms, Memory: 2100 KB -BaekjoonHub

---
+++ C++17/백준/Gold/25953. 템포럴 그래프/템포럴 그래프.cc
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+
+using namespace std;
+
+int n, t, m;
+int si, ei;
+int dists[10001][2];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    memset(dists, -1, sizeof(dists));
+    
+    cin >> n >> t >> m;
+    cin >> si >> ei;
+    dists[si][0] = 0;
+    dists[si][1] = 0;
+    for(int i=1;i<=t;i++){
+        int prevDistIndex = i % 2;
+        int distIndex = 1 - prevDistIndex;
+        for(int j=0;j<m;j++){
+            int s, e, cost;
+            cin >> s >> e >> cost;
+            
+            if(dists[s][prevDistIndex] != -1){
+                int temp = dists[s][prevDistIndex] + cost;
+                if(dists[e][prevDistIndex] == -1 || dists[e][prevDistIndex] > temp){
+                    dists[e][distIndex] = temp;
+                }
+            }
+            
+            if(dists[e][prevDistIndex] != -1){
+                int temp = dists[e][prevDistIndex] + cost;
+                if(dists[s][prevDistIndex] == -1 || dists[s][prevDistIndex] > temp){
+                    dists[s][distIndex] = temp;
+                }
+            }
+        }
+        
+        for(int i=0;i<10001;i++){
+            dists[i][prevDistIndex] = dists[i][distIndex];
+        }
+    }
+    
+    int result = dists[ei][0];
+    if(result > dists[ei][1]){
+        result = dists[ei][1];
+    }
+    
+    cout << result << "\n";
+    
+    return 0;
+}
